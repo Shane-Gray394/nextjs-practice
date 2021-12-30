3ced10642187256b302b7ab990e5debed2f648d5
Shane-Gray394 <86175936+Shane-Gray394@users.noreply.github.com>
2021-12-30 16:03:40 -0500

    wip

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -26,7 +26,7 @@ const NewMeetupPage = () => {
           content="Add your own meetups and create amazing networking opportunities"
         />
       </Head>
-      <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </Fragment>
   );
 };
